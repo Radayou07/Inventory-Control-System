827501c8d99f97bff5a62e63d39e3feb86d20018
RA Dayou <bora.dayou@gmail.com>
2026-03-17 18:14:01 +0700

    will it work

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
 const express = require('express');
-const  mysql = require('mysql2');
-require('dotenv').config();
+const app = express();
 
-const app = express
-
-// connect to HTML
+app.use(express.json());
 app.use(express.static('public'));
 
-// Connect to Database
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASWWORD,
-    database: process.env.DB_NAME
-})
-
-// Check connection
-db.connect((err) => {
-    if(err) {
-        console.log("Connection fail", err.message);
-        return;
-    }
-    console.log("Connection success!")
-})
-
-app.length("/product")
-
+// plug in all routes
+app.use('/products', require('./routes/products'));
+app.use('/customers', require('./routes/customers'));
+app.use('/employees', require('./routes/employees'));
+ 
+app.listen(process.env.PORT || 3000, () => console.log("running!"));
